7bce2a17fdf4320b2c677cb4fa3a4dd5b472489a
Prateek <prateektomar005@gmail.com>
2025-01-31 00:07:45 +0530

    Implemented-UI-Selection-Panel

---
+++ Assets/Scripts/TankSelection.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSelection : MonoBehaviour
+{
+    public TankSpawner tankSpawner;
+
+    public void BlueTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.BlueTank);
+    }
+
+    public void GreenTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.GreenTank);
+
+    }
+
+    public void RedTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.RedTank);
+
+    }
+}
--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -19,13 +19,27 @@ public class TankSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CreateTank();
+        
     }
 
-    private void CreateTank()
+    public void CreateTank(TankTypes tankTypes)
     {
-        TankModel tankModel = new TankModel(tankList[0].movementSpeed, tankList[0].rotationSpeed, tankList[0].tankTypes, tankList[0].color);
-        TankController tankController = new TankController(tankModel,tankView);
+        if (tankTypes == TankTypes.GreenTank)
+        {
+            TankModel tankModel = new TankModel(tankList[0].movementSpeed, tankList[0].rotationSpeed, tankList[0].tankTypes, tankList[0].color);
+            TankController tankController = new TankController(tankModel, tankView);
+        }
+        else if (tankTypes == TankTypes.BlueTank)
+        {
+            TankModel tankModel = new TankModel(tankList[1].movementSpeed, tankList[1].rotationSpeed, tankList[1].tankTypes, tankList[1].color);
+            TankController tankController = new TankController(tankModel, tankView);
+        }
+        else if (tankTypes == TankTypes.RedTank)
+        {
+            TankModel tankModel = new TankModel(tankList[2].movementSpeed, tankList[2].rotationSpeed, tankList[2].tankTypes, tankList[2].color);
+            TankController tankController = new TankController(tankModel, tankView);
+        }
+        
         
     }
     
